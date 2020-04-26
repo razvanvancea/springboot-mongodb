5ec1c0c08e5bf0fe02662e1f0514a92fbdcfe022
Razvan Vancea <rvancea@deloittece.com>
2020-04-27 00:03:55 +0300

    include swagger annotations

--- src/main/java/com/rv/springbootmongodb/controller/PersonController.java
+++ src/main/java/com/rv/springbootmongodb/controller/PersonController.java
@@ -2,6 +2,8 @@ package com.rv.springbootmongodb.controller;
 
 import com.rv.springbootmongodb.model.Person;
 import com.rv.springbootmongodb.service.PersonService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
@@ -9,6 +11,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api")
+@Api(value = "Person Controller")
 public class PersonController {
 
     private PersonService personService;
@@ -19,18 +22,21 @@ public class PersonController {
 
     @GetMapping("/persons")
     @ResponseStatus(HttpStatus.OK)
+    @ApiOperation(value = "returns persons list")
     public List<Person> getPersons() {
         return personService.getAllPersons();
     }
 
     @PostMapping("/persons")
     @ResponseStatus(HttpStatus.CREATED)
+    @ApiOperation(value = "adds a new person")
     public void createPerson(@RequestBody Person p) {
         personService.createPerson(p);
     }
 
     @DeleteMapping("/persons/{id}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
+    @ApiOperation(value = "deletes a person by ID")
     public void deletePerson(@PathVariable String id) {
         personService.deletePersonById(id);
     }
--- src/main/java/com/rv/springbootmongodb/model/Person.java
+++ src/main/java/com/rv/springbootmongodb/model/Person.java
@@ -1,5 +1,6 @@
 package com.rv.springbootmongodb.model;
 
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Builder;
 import lombok.Data;
 import org.springframework.data.annotation.Id;
@@ -10,7 +11,10 @@ import org.springframework.data.mongodb.core.mapping.Document;
 @Builder
 public class Person {
     @Id
+    @ApiModelProperty(value = "the person ID")
     private String id;
+    @ApiModelProperty(value = "the first name")
     private String firstName;
+    @ApiModelProperty(value = "the last name")
     private String lastName;
 }
